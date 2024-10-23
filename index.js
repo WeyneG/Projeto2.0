b07e682d372480ea75e391dc29d0368b0e8ddd3c
haphaak <carlosdirceu519@gmail.com>
2024-10-23 17:51:20 -0300

    nv codigo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>Iai professor, bem vindo ao meu site!</h1>");
+    res.send("<h1>olá professor, bem vindo ao meu site!</h1>");
 })
 
 //rota do cadastro de produtos
